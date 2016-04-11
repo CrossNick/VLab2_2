0bdea3c9701e3a954aa394654380163974fe82bf
CrossNick <nagatopeih@yandex.ru>
2016-04-11 18:05:46 +0300

    New functions
    Added converttToDex method and Num property

--- VLab2_2/Program.cs
+++ VLab2_2/Program.cs
@@ -8,18 +8,54 @@ namespace VLab2_2
 {
     class Oct
     {
-        string num;
-        int toDex()
-        {
-            int dex=0;
+        private string num;
 
+        public int toDex()
+        {
+            int dex=0, octx=Convert.ToInt32(num);
+            int len = num.Length;
+            for(int i = 0;i< len;i++)
+            {
+                dex += (octx%10) * Convert.ToInt32(Math.Pow(8, i));
+                octx /= 10;
+            }
             return dex;
+        }
+
+        public string Num
+        {
+            get
+            {
+                return num;
+            }
+            set
+            {
+                bool chk = true;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (Convert.ToInt32(value[i]) == 8 || Convert.ToInt32(value[i]) == 9)
+                    {
+                        chk = false;
+                    }
+                }
+                if (chk == true)
+                    num = value;
+                else
+                    throw new FormatException();
+            }
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Oct oct = new Oct();
+            Console.WriteLine("Введите восьмеричное число:");
+            oct.Num = Console.ReadLine();
+            int dex = oct.toDex();
+            Console.WriteLine("Число в десятично форме:");
+            Console.WriteLine(dex);
+
         }
     }
 }
