27656b379ef67687684ba7c8847ccc0de8cfdc47
CrossNick <nagatopeih@yandex.ru>
2016-04-11 17:34:17 +0300

    Created project

---
+++ VLab2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLab2_2
+{
+    class Oct
+    {
+        string num;
+        int toDex()
+        {
+            int dex=0;
+
+            return dex;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
